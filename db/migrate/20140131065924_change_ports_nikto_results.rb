1bd115a7db8033464b784608b68a7f7f8b1d30b8
Peter McLarnan <peter@matasano.com>
2014-04-10 18:19:04 -0500

    make ChangePortsNiktoResults reversible

--- db/migrate/20140131065924_change_ports_nikto_results.rb
+++ db/migrate/20140131065924_change_ports_nikto_results.rb
@@ -1,5 +1,9 @@
 class ChangePortsNiktoResults < ActiveRecord::Migration
-  def change
+  def up
     change_column :ports, :nikto_results, :text, :limit => 2147483647
+  end
+
+  def down
+    change_column :ports, :nikto_results, :text
   end
 end
