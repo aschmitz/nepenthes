35c5bd815a707fefa6d0ac6346f5bb838c9dab23
Peter McLarnan <peter@matasano.com>
2014-04-10 18:19:36 -0500

    hash screenshots in AddDataHashToScreenshot

--- db/migrate/20140130074552_add_data_hash_to_screenshot.rb
+++ db/migrate/20140130074552_add_data_hash_to_screenshot.rb
@@ -1,5 +1,17 @@
 class AddDataHashToScreenshot < ActiveRecord::Migration
+  class Screenshot < ActiveRecord::Base
+  end
+
   def change
     add_column :screenshots, :data_hash, :string
+    Screenshot.reset_column_information
+    reversible do |dir|
+      dir.up do
+        Screenshot.where.not(data: nil).find_each do |x|
+          x.data_hash = OpenSSL::Digest::MD5.hexdigest x.data
+          x.save!
+        end
+      end
+    end
   end
 end
