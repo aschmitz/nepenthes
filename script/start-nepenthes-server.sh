3f7e2b6bfa9b73d48a5fc26f83dddddd34d0649f
Andy Schmitz <aschmitz@matasano.com>
2014-08-29 12:30:53 -0500

    Add batch worker to sprout start script.

--- script/start-nepenthes-server.sh
+++ script/start-nepenthes-server.sh
@@ -10,5 +10,7 @@ tmux new-session -s nepenthesserver -n unicorn -d \
   'unicorn_rails -E production'
 tmux new-window -t nepenthesserver:1 -n results \
   'sidekiq -e production -c 5 -q results -v'
+tmux new-window -t nepenthesserver:1 -n batch \
+  'sidekiq -e production -c 1 -q batch -v'
 
 echo -e "\n[*] Go to http://`hostname`:8080/ to load Nepenthes."
