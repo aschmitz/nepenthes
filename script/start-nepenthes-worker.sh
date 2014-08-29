85b956299af9068dc30097b4ebcc78f1161ec340
Andy Schmitz <aschmitz@matasano.com>
2014-08-29 11:03:20 -0500

    Make Redis check more robust on workers.
    This may or may not fix the issue, as I couldn't reproduce it locally.

--- script/start-nepenthes-worker.sh
+++ script/start-nepenthes-worker.sh
@@ -6,7 +6,7 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 echo -e "\n[*] Checking for Redis..."
-echo -en "PING\r\nQUIT\r\n" | ncat -w 5 127.0.0.1 6379 | grep -q PONG
+echo -en "PING\r\nQUIT\r\n" | ncat -w 5s -d 5s 127.0.0.1 6379 | grep -q PONG
 if [ $? -ne 0 ]; then
   echo "Couldn't detect Redis on localhost." \
     "Try SSHing to this worker like this:"
