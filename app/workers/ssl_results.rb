e393a2abb38caa81a1a5126a59532e7e51933f57
00damian <damian.archer@hotmail.co.uk>
2014-10-29 15:41:22 -0500

    Update ssl_results.rb
    Added certificate expiration check.

--- app/workers/ssl_results.rb
+++ app/workers/ssl_results.rb
@@ -1,4 +1,5 @@
 require 'openssl'
+require 'date'
 
 CERT_REGEX = /-----BEGIN CERTIFICATE-----[^-]*-----END CERTIFICATE-----/
 
@@ -14,8 +15,14 @@ class SslResults
     
     if cert_match = ssl_data.match(CERT_REGEX)
       cert = OpenSSL::X509::Certificate.new(cert_match[0])
+      expiry_date = cert.not_after.to_s
       port.notes ||= ''
       # We add a space after slashes to wrap long lines.
+      if Date.parse(expiry_date) > Date.today
+        port.notes += "Certificate is valid\n"
+      else
+        port.notes += "Certificate is out of date\n"
+      end
       port.notes += "SSL For: #{cert.subject.to_s.gsub('/', '/ ')}\n"
       cert.extensions.each do |extension|
         if extension.oid == 'subjectAltName'
@@ -26,3 +33,5 @@ class SslResults
     port.save!
   end
 end
+
+
