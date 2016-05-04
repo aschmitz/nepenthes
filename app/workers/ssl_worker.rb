703f1a51cc0c8202877e7348f812665d065970b4
Andy Schmitz <andy.schmitz@gmail.com>
2016-05-04 15:29:23 -0500

    Force valid UTF-8 in SSL results.

--- app/workers/ssl_worker.rb
+++ app/workers/ssl_worker.rb
@@ -10,6 +10,7 @@ class SslWorker
     end
     ssl_data, status = Open3.capture2($TIMEOUT_PATH, '2', $OPENSSL_PATH,
       's_client', '-connect', "#{host}:#{port}", '-showcerts')
+    ssl_data.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace)
     Sidekiq::Client.enqueue(SslResults, id, ssl_data)
   end
 end
