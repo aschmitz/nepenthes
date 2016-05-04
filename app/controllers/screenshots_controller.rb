629b1ee14f1715564096cb945eebc685d01614e4
Andy Schmitz <andy.schmitz@gmail.com>
2016-05-04 15:28:36 -0500

    Hide screenshots that haven't yet been taken.

--- app/controllers/screenshots_controller.rb
+++ app/controllers/screenshots_controller.rb
@@ -1,12 +1,14 @@
 class ScreenshotsController < ApplicationController
   def index
     if params[:distinct] == 'true'
-      @screenshots = Screenshot.group('data_hash').page(params[:page]).per(100)
+      @screenshots = Screenshot.group('data_hash')
     else
-      @screenshots = Screenshot.order('data_hash').page(params[:page]).per(100)
+      @screenshots = Screenshot.order('data_hash')
     end
+    @screenshots = @screenshots.where(
+      'data_hash IS NOT NULL').page(params[:page]).per(100)
   end
-
+  
   def show
     screenshot = Screenshot.find_by_id(params[:id])
     send_data screenshot.data, type: 'image/png', disposition: 'inline'
