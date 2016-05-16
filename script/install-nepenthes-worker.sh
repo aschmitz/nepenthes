2849665b8b1f2048b8308fa91c65b7e0c1daa4c8
Andy Schmitz <andy.schmitz@gmail.com>
2016-05-16 12:14:41 -0500

    Force installing PhantomJS from binaries

--- script/install-nepenthes-worker.sh
+++ script/install-nepenthes-worker.sh
@@ -32,29 +32,26 @@ else
   PACKAGES="$PACKAGES ruby ruby-dev"
 fi
 
-if [ $UBUNTU_VERSION \< "12.10" ]; then
-  echo -e "\n[*] Using Ubuntu < 12.10, downloading PhantomJS separately."
-  arch=`uname -m`
-  if [[ $arch == "x86_64" ]]; then
-    phantomjshash=c78c4037d98fa893e66fc516214499c58228d2f9
-  else
-    phantomjshash=9ead5dd275f79eaced61ce63dbeca58be4d7f090
-  fi
-  wget -O phantomjs.tar.bz2 \
-    https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-$arch.tar.bz2
-  sha1sum phantomjs.tar.bz2 | grep -q $phantomjshash
-  if [ $? -ne 0 ]; then
-    echo "Unexpected PhantomJS SHA-1 hash. Please try again."
-    exit 1
-  fi
-  echo -e "\n[*] Extracting PhantomJS..."
-  tar xjf phantomjs.tar.bz2
-  echo -e "\n[*] Installing PhantomJS..."
-  cp phantomjs-*/bin/phantomjs /bin/
+echo -e "\n[*] Downloading PhantomJS separately."
+arch=`uname -m`
+if [[ $arch == "x86_64" ]]; then
+  phantomjshash=86dd9a4bf4aee45f1a84c9f61cf1947c1d6dce9b9e8d2a907105da7852460d2f
 else
-  echo -e "\n[*] Using Ubuntu >= 12.10, using the repository's PhantomJS."
-  PACKAGES="$PACKAGES phantomjs"
+  phantomjshash=80e03cfeb22cc4dfe4e73b68ab81c9fdd7c78968cfd5358e6af33960464f15e3
 fi
+# Yes, this isn't an official source, but we're checking the hashes.
+# Bitbucket (the current official source) seems to block automatic downloads.
+wget -O phantomjs.tar.bz2 \
+  https://github.com/paladox/phantomjs/releases/download/2.1.7/phantomjs-2.1.1-linux-$arch.tar.bz2
+sha256sum phantomjs.tar.bz2 | grep -q $phantomjshash
+if [ $? -ne 0 ]; then
+  echo "Unexpected PhantomJS SHA-1 hash. Please try again."
+  exit 1
+fi
+echo -e "\n[*] Extracting PhantomJS..."
+tar xjf phantomjs.tar.bz2
+echo -e "\n[*] Installing PhantomJS..."
+cp phantomjs-*/bin/phantomjs /bin/
 
 echo -e "\n[*] Checking multiverse availability"
 grep -q '^\s*deb.*multiverse' /etc/apt/sources.list
